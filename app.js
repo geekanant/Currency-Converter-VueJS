f471d0f485264213f0b633f5232efdec5decec7b
anantpatni1 <anantpatni1@gmail.com>
2018-12-29 20:03:30 +0530

    Added Convert Functionality

--- app.js
+++ app.js
@@ -1,14 +1,54 @@
 new Vue({
     el:'#app',
     data:{
-        currencies:{}
+        currencies:{},
+        amount:null,
+        from:'USD',
+        to:'INR',
+        result: 0
     },
 
     mounted(){
-        axios.get('https://free.currencyconverterapi.com/api/v6/currencies')
-        .then(response=>{
-            console.log(response);
-            this.currencies=response.data.results;
-        });
+        this.getCurrencies();
+    },
+    
+    computed:{
+        formattedCurrencies(){
+            return Object.values(this.currencies);
+        },
+
+        calculateResult(){
+            return (Number(this.amount)*this.result).toFixed(3 );
+        }
+    },
+
+    methods: {
+        getCurrencies() {
+
+            const currencies=localStorage.getItem('currencies')
+
+            if(currencies){
+                this.currencies=JSON.parse(currencies);
+
+                return;
+            }
+
+            axios.get('https://free.currencyconverterapi.com/api/v6/currencies')
+            .then(response=>{
+                this.currencies=response.data.results;
+                localStorage.setItem('currencies',JSON.stringify(response.data.results))
+            });
+        },
+
+        convertCurrency() {
+
+            const key = `${this.from}_${this.to}`;
+
+            axios.get(`https://free.currencyconverterapi.com/api/v6/convert?q=${key}`)
+            .then((response)=>{
+                console.log(response)
+                this.result = response.data.results[key].val
+            })
+        }
     }
 })
